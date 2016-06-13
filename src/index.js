7d61076400a7137bd5448a5fdb61028a833d161c
Doug Modie <dmodie@gmail.com>
2016-06-13 18:41:22 -0500

    we are doing a search and displayng length in renderer of video_list class

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search'
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCeVnCFcPcECUGSfdfDuRfcAbtaOCUT6Vc';
+
+YTSearch ({key: API_KEY, term: 'surfboards'}, function (data){
+    console.log (data)
+})
 //APIKey = AIzaSyCeVnCFcPcECUGSfdfDuRfcAbtaOCUT6Vc
-const App =  () =>{
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+class App extends Component {
+    constructor (props){
+        super (props);
+        YTSearch ({key: API_KEY, term: 'surfboards'},  (videos) =>{
+           // console.log (data)
+            this.setState({videos});
+        });
+
+        this.state = {videos: []};
+    };
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos= {this.state.videos}/>
+
+            </div>
+        );
+    }
 }
+//const App =  () =>{
+//    return (
+//        <div>
+//            <SearchBar />
+//        </div>
+//    );
+//}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
