e378a4cfec41fded8a9cb2014692b39333a571fb
Doug Modie <dmodie@gmail.com>
2016-06-12 23:04:17 -0500

    displaying search bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyCeVnCFcPcECUGSfdfDuRfcAbtaOCUT6Vc'
+const API_KEY = 'AIzaSyCeVnCFcPcECUGSfdfDuRfcAbtaOCUT6Vc';
 //APIKey = AIzaSyCeVnCFcPcECUGSfdfDuRfcAbtaOCUT6Vc
 const App =  () =>{
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
-ReactDOM.render(<App />, document.querySelector(".container"))
+ReactDOM.render(<App />, document.querySelector(".container"));
