107700e981b52a16c8e3341595cef0b2ea752de2
Doug Modie <dmodie@gmail.com>
2016-06-13 17:10:23 -0500

    set up state in class, and data binding between state and input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
+
+class SearchBar extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = { term:''}
+    }
+
+    render (){
+      return (
+          <div>
+            <input
+                value = {this.state.term}
+                onChange = {event => this.setState ({ term: event.target.value })}/>
+              Value of input: {this.state.term}
+          </div>
+
+          //<div>
+          //  <input onChange = {this.onInputChange}/>
+          //  Value of input: {this.state.term}
+          //
+          //</div>
+      );
+    }
+    //
+    //
+    //onInputChange(event){
+    //    console.log ("event " + event)
+    ////    this.setState ({term: event.target.value})
+    ////    console.log (event.target.value)
+    //}
+
+
+
+
+
 }
 
 export default SearchBar
